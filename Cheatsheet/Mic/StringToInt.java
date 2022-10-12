6fbb946e60d4882ae4b00736d00a94a0d3385a97
aditriz <41908466+aditriz@users.noreply.github.com>
2022-10-12 22:02:10 +0700

    Update StringToInt.java

--- Cheatsheet/Mic/StringToInt.java
+++ Cheatsheet/Mic/StringToInt.java
@@ -1,9 +1,16 @@
 package Mic;
 
+import java.util.*;
+
 public class StringToInt {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
         String str = "123";
         int num = Integer.parseInt(str);
         String st = Integer.toString(num);
+
+        String a = in.nextLine();
+		String[] aArr = a.split(" ");
     }
 }
