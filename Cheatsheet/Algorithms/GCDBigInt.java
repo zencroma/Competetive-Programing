e3aa92a738bd9e89ffa69a13439ad7efd2e55524
aditriz <41908466+aditriz@users.noreply.github.com>
2022-10-12 21:07:38 +0700

    Create GCDBigInt.java

---
+++ Cheatsheet/Algorithms/GCDBigInt.java
@@ -0,0 +1,32 @@
+package Algorithms;
+
+// Java program to demonstrate how 
+// to use gcd method of BigInteger class 
+
+import java.math.BigInteger; 
+
+public class GCDBigInt { 
+	public static int gcd(int a, int b) 
+	{ 
+		BigInteger b1 = BigInteger.valueOf(a); 
+		BigInteger b2 = BigInteger.valueOf(b); 
+		BigInteger gcd = b1.gcd(b2); 
+		return gcd.intValue(); 
+	} 
+
+	public static long gcd(long a, long b) 
+	{ 
+		BigInteger b1 = BigInteger.valueOf(a); 
+		BigInteger b2 = BigInteger.valueOf(b); 
+		BigInteger gcd = b1.gcd(b2); 
+		return gcd.longValue(); 
+	} 
+
+	// Driver method 
+	public static void main(String[] args) 
+	{ 
+		System.out.println(gcd(3, 5)); 
+		System.out.println(gcd(10000000000L, 600000000L)); 
+	} 
+}
+
